c1532225dd432a5398f747a4832f559de351cb08
ethan1022 <ethan1022@gmail.com>
2018-09-21 17:58:13 +0800

    Initial Commit

---
+++ SiriShortcutTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcutTest
+//
+//  Created by Ethan on 2018/9/21.
+//  Copyright © 2018 Ethan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
