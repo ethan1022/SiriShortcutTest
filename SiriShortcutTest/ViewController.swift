01189f7ed762ff3bf667cfa2aeef8dc738934e13
ethan1022 <ethan1022@gmail.com>
2018-09-21 18:42:11 +0800

    my first commit

--- SiriShortcutTest/ViewController.swift
+++ SiriShortcutTest/ViewController.swift
@@ -12,7 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setupIntents()
+    }
+    
+    func setupIntents() {
+        let activity = NSUserActivity(activityType: "com.ethan.SiriShortcutTest.你好")
+        activity.title = "你好"
+        activity.userInfo = ["speech":"hi"]
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+        activity.persistentIdentifier = "com.ethan.SiriShortcutTest.你好"
+        self.view.userActivity = activity
+        activity.becomeCurrent()
+    }
+    
+    public func sayHi() {
+        let alert = UIAlertController(title: "Hi There!", message: "Hey there! Glad to see you got this working!", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 
 
